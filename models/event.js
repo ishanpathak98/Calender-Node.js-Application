7ca027dd2962ed455d7950a466c31d84130f8c5f
ishanpathak98 <95092310+ishanpathak98@users.noreply.github.com>
2024-10-10 15:11:53 +0530

    Update event.js

--- models/event.js
+++ models/event.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const EventSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  date: Date,
+  title: { type: String, required: true },
+  description: { type: String },
+  date: { type: Date, required: true }
 });
 
 module.exports = mongoose.model('Event', EventSchema);
+
