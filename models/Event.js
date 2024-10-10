e70e5c8e434f34dc51a111619357fa73878e9a17
Ubuntu <ubuntu@ip-172-31-4-100.us-west-2.compute.internal>
2024-10-10 10:43:11 +0000

    Fix: resolved conflicts in Event.js

--- models/Event.js
+++ models/Event.js
@@ -1,15 +1,5 @@
 const mongoose = require('mongoose');
 
-const eventSchema = new mongoose.Schema({
-<<<<<<< HEAD
-    title: String,
-    description: String,
-    date: Date
-});
-
-module.exports = mongoose.model('Event', eventSchema);
-const mongoose = require('mongoose');
-
 const EventSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
@@ -17,12 +7,4 @@ const EventSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Event', EventSchema);
-=======
-    title: { type: String, required: true },
-    description: { type: String },
-    date: { type: Date, required: true }
-});
-
-module.exports = mongoose.model('Event', eventSchema);
->>>>>>> 3934658 (changes)
 
