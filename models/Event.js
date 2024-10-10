f93bdb5a48b65506946ef0faa1562ab3df7b227c
Ubuntu <ubuntu@ip-172-31-4-100.us-west-2.compute.internal>
2024-10-10 10:20:47 +0000

    modification

--- models/Event.js
+++ models/Event.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 
+const eventSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    date: Date
+});
+
+module.exports = mongoose.model('Event', eventSchema);
+const mongoose = require('mongoose');
+
 const EventSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
