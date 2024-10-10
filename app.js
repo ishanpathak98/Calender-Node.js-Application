6f8fcb800d8aab94fcf69c1bbced29a5547ada0f
ishanpathak98 <95092310+ishanpathak98@users.noreply.github.com>
2024-10-10 14:04:32 +0530

    Update app.js
    added the route

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const eventRoutes = require('./routes/eventRoutes');
+app.use('/api', eventRoutes);
 const app = express();
 app.use(bodyParser.json());
 
